0d3ea6feaa73e0417b07b1c716807c24f2e1d7a7
Giulia <giu.tondin@gmail.com>
2021-02-19 12:00:32 -0300

    Changed home elements

--- src/app/app-menu/app-menu.component.ts
+++ src/app/app-menu/app-menu.component.ts
@@ -11,13 +11,29 @@ export class AppMenuComponent implements OnInit {
 
   links = [
     {
-      title: 'About'
+      title: 'Home',
+      page: '/'
     },
     {
-      title: 'Work'
+      title: 'Work',
+      page: '/work'
     },
     {
-      title: 'Contact'
+      title: 'Contact',
+      page: '/contact'
+    }
+  ]
+
+  socials = [
+    { 
+      icon: '../../assets/imgs/linkedin.svg',
+      link: 'https://www.linkedin.com/in/giulia-tondin-621290145/',
+      link_title: 'Open Linkedin profile' 
+    },
+    {
+      icon: '../../assets/imgs/github.svg',
+      link: 'https://github.com/giuliatondin',
+      link_title: 'Open Github profile'
     }
   ]
 
